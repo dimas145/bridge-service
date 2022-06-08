0d51e7b0f4184882c2642af995faa2404287710b
dimas145 <13518069@std.stei.itb.ac.id>
2022-06-08 12:28:30 +0700

    add API get running autograders

---
+++ src/Controller/Autograder/getRunning.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+import { DockerStatus } from '../../Type/Docker'
+import { Autograder } from '../../Model/Autograder'
+
+export async function GetRunning(_: Request, res: Response) {
+    const autograders = await Autograder.find({ status: DockerStatus.RUNNING })
+
+    return res.send({
+        success: true,
+        autograders: autograders.map((grader) => grader.name)
+    })
+}
--- src/Routes/autograder.ts
+++ src/Routes/autograder.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { Start } from '../Controller/Autograder/start'
 import { Initialize } from '../Controller/Autograder/initialize'
+import { GetRunning } from '../Controller/Autograder/getRunning'
 import { RequestWrapper } from '../Utils/requestWrapper'
 
 const autograderRoute = express.Router()
 
+autograderRoute.get('/running', RequestWrapper(GetRunning))
 autograderRoute.post('/start', RequestWrapper(Start))
 autograderRoute.post('/initialize', RequestWrapper(Initialize))
 
