846f5fab761b4e27d09e9380948a52a5b681a176
dimas145 <13518069@std.stei.itb.ac.id>
2022-07-11 15:51:31 +0700

    update moodle web service request

--- src/Controller/Gitlab/updateMoodle.ts
+++ src/Controller/Gitlab/updateMoodle.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { Student } from '../../Model/Student'
 import { Constant } from '../../constant'
 
-export async function updateMoodle(req: Request, res: Response){
+export async function updateMoodle(req: Request, res: Response) {
     const userId = JSON.parse(req.query['state']?.toString() || '{}')?.['userId']
 
     const model = Student.create({
@@ -15,36 +15,44 @@ export async function updateMoodle(req: Request, res: Response){
     try {
         await model.save()
     } catch (error) {
-        await axios.get(process.env.MOODLE_HOST + '/webservice/rest/server.php', {
+        try {
+            await axios.get(process.env.MOODLE_HOST as string + '/webservice/rest/server.php', {
+                params: {
+                    'moodlewsrestformat': 'json',
+                    'wstoken': process.env.MOODLE_TOKEN,
+                    'wsfunction': Constant.WS_FUNCTION_UPDATE_USER,
+                    'users[0][id]': userId,
+                    'users[0][customfields][0][type]': 'gitlabUsername',
+                    'users[0][customfields][0][value]': req.session.passport.user.username,
+                    'users[0][customfields][1][type]': 'isGitLabVerified',
+                    'users[0][customfields][1][value]': 1
+                }
+            })
+            return res.redirect(process.env.MOODLE_HOST as string + '/user/profile.php')
+        } catch (error) {
+            return res.send(error)
+        }
+    }
+
+    try {
+        const resp = await axios.get(process.env.MOODLE_HOST + '/webservice/rest/server.php', {
             params: {
                 'moodlewsrestformat': 'json',
                 'wstoken': process.env.MOODLE_TOKEN,
                 'wsfunction': Constant.WS_FUNCTION_UPDATE_USER,
                 'users[0][id]': userId,
-                'users[0][customfields][0][type]': 'gitlab',
+                'users[0][customfields][0][type]': 'gitlabUsername',
                 'users[0][customfields][0][value]': req.session.passport.user.username,
-                'users[0][customfields][1][type]': 'isGitlabVerified',
+                'users[0][customfields][1][type]': 'isGitLabVerified',
                 'users[0][customfields][1][value]': 1
             }
         })
-        return res.send('you can close this page')
-    }
-
-    const resp = await axios.get(process.env.MOODLE_HOST + '/webservice/rest/server.php', {
-        params: {
-            'moodlewsrestformat': 'json',
-            'wstoken': process.env.MOODLE_TOKEN,
-            'wsfunction': Constant.WS_FUNCTION_UPDATE_USER,
-            'users[0][id]': userId,
-            'users[0][customfields][0][type]': 'gitlab',
-            'users[0][customfields][0][value]': req.session.passport.user.username,
-            'users[0][customfields][1][type]': 'isGitlabVerified',
-            'users[0][customfields][1][value]': 1
+        if (resp.status === 200) {
+            return res.redirect(process.env.MOODLE_HOST as string + '/user/profile.php')
+        } else {
+            return res.send('something error')
         }
-    })
-    if (resp.status === 200){
-        return res.send('you can close this page')
-    } else{
-        return res.send('something error')
+    } catch (error) {
+        return res.send(error)
     }
 }
