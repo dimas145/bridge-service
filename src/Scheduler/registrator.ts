64f558552fca374a8822632e179cf1569720b1e9
dimas145 <13518069@std.stei.itb.ac.id>
2022-07-17 10:31:20 +0700

    remove delete autograder on shutdown

--- src/Scheduler/registrator.ts
+++ src/Scheduler/registrator.ts
@@ -1,5 +1,4 @@
 import { ToadScheduler, LongIntervalJob, AsyncTask } from 'toad-scheduler'
-import { getConnectionManager } from 'typeorm'
 import { DockerStatus } from '../Type/Docker'
 import { Autograder } from '../Model/Autograder'
 import { Constant } from '../constant'
@@ -57,39 +56,3 @@ export const registratorScheduler = (): ToadScheduler => {
     scheduler.addLongIntervalJob(job)
     return scheduler
 }
-
-// handle exit service, stop and remove all running autograder container
-async function exitHandler(eventType: any) {
-    console.log('clean up before exiting')
-    if (getConnectionManager().connections.length > 0 && (eventType || eventType === 0)) {
-        try {
-            // clean up autograder table
-            const allAutograder = await Autograder.find()
-
-            for (let i = 0; i < allAutograder.length; i++) {
-                const autograder = allAutograder[i]
-                try {
-                    autograder.status = DockerStatus.STOPPED
-                    const container = docker.getContainer(autograder.containerId as string)
-                    await container.stop()
-
-                    autograder.containerId = null
-                } catch (err) {
-                    console.error(`error killing container with id ${autograder.containerId}`)
-                    console.error(err)
-                }
-            }
-
-            await Autograder.save(allAutograder)
-            console.log('clean up done')
-        } catch (err) {
-            console.error(err)
-        }
-
-        console.log(`process exit with eventType: ${eventType}`)
-    }
-}
-
-['exit', 'SIGINT', 'SIGUSR1', 'SIGUSR2', 'uncaughtException', 'SIGTERM'].forEach((eventType) => {
-    process.on(eventType, exitHandler.bind(null, eventType))
-})
